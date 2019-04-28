daf4f193e5b55a0a2a13df216be142e84faab7ee
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-04-28 14:17:23 -0300

    [UPDATE] - Implementando o menu de itens.

---
+++ lib/menu/MenuCards.dart
@@ -0,0 +1,8 @@
+import 'package:hush_talk/model/CardModel.dart';
+
+final menuCards = <CardModel>[
+  CardModel(
+    title: "DBZ",
+    avatar: "https://brand.campaign.adidas.com/Images/originals-fw18-dragonball-mh-d-v1_tcm66-283071.jpg?locale=en_US&device=mobile&version=6",
+  ),
+];
