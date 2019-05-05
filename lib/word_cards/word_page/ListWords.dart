32e55b10335e279c1af2f25f251972347ea6c662
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-05-05 20:56:38 -0300

    [UPDATE] - Corrigindo responsividade

--- lib/word_cards/word_page/ListWords.dart
+++ lib/word_cards/word_page/ListWords.dart
@@ -21,7 +21,6 @@ class ListWords extends StatelessWidget {
     _controller.moveDown(height);
     return Column(children: <Widget>[
       Container(
-        height: 105,
         color: Colors.white,
         child: Center(
           child: Align(
@@ -61,7 +60,7 @@ class ListWords extends StatelessWidget {
           ),
         ),
       ),
-      Expanded(
+      Flexible(
         child: ListView.builder(
           controller: _controller,
           itemCount: _controller.cardList.length,
